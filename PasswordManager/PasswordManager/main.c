71ca258ae04429c667ca24797d05ec41ba891b92
Blaggan13 <Tblaggan4502@conestogac.on.ca>
2022-03-12 15:20:51 -0500

    Password Generator

--- PasswordManager/PasswordManager/main.c
+++ PasswordManager/PasswordManager/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-	printf("Hello World!\n");
+int main()
+{
+	    printf_s("_____                                     _   __  __");
+		printf_s("| __ \                                   | | |  \/  |");
+		printf_s("||__) |_  _ ___ _____      _____  _ __ __| | | \  / | __ _ _ __   __ _  __ _  ___ _ __ ");
+		printf_s("|  ___/ _` / __/ __\ \ /\ / / _ \| '__/ _` | | |\/| |/ _` | '_ \ / _` |/ _` |/ _ \ '__| ");
+		printf_s("| |  | (_| \__ \__ \\ V  V / (_) | | | (_| | | |  | | (_| | | | | (_| | (_| |  __/ |    ");
+		printf_s("|_|   \__,_|___/___/ \_/\_/ \___/|_|  \__,_| |_|  |_|\__,_|_| |_|\__,_|\__, |\___|_|    ");
+        printf_s("                                                                  __/ |           ");
+		printf_s("                                                                 |___/            ");
+
+
+
+
+	int length;
+	// Function Call
+	randomPasswordGeneration(length);
+
 	return 0;
 }
