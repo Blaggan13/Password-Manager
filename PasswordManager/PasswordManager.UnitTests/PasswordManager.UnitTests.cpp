bab3cb4e89a05e56b8ea518950267ac9fe748850
Artem Maksymov <armaksimov3@gmail.com>
2022-03-25 20:58:38 -0400

    Testing project added to solution. Depedencies established.

---
+++ PasswordManager/PasswordManager.UnitTests/PasswordManager.UnitTests.cpp
@@ -0,0 +1,48 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PasswordManagerUnitTests
+{
+	TEST_CLASS(PasswordTests)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+	TEST_CLASS(AppllicationTests)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+	TEST_CLASS(EncryptionTests)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+	TEST_CLASS(FileIOTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+	TEST_CLASS(PasswordManagerTests)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
